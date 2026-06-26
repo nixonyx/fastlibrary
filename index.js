4b8023c9757d9ec42aa5adec93b34b53e74df096
nixonyx <nixonhen7@outlook.com>
2026-06-26 12:00:20 -0400

    Criado o servidor inicial com express porta 3000

---
+++ index.js
@@ -0,0 +1,18 @@
+//importar o express 
+const express = require('express')
+
+//criação  do app express
+const app = express()
+
+//porta do servidor
+const PORT = 300;
+
+//rota de caminho do servidor
+app.get('/', (req, res) => {
+    res.send('Servidor FastLibrary está rodando com sucesso!');
+});
+
+//ligando o servidor
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}! Acesse: http://localhost:${PORT}`);
+});
